93c5448e00f363568b868abc1b2569b2772b5fc0
Fixer <fixer3838@live.fr>
2020-10-15 23:23:33 +0200

    ADDED DLL library Halo

---
+++ Halo/src/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace Halo {
+	void Print()
+	{
+		printf("Welcome to Halo game engine.");
+	}
+}
---
+++ Halo/src/Test.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Halo {
+	__declspec(dllexport) void Print();
+}
